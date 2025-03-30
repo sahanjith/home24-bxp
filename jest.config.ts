ad69e8f6536332e7e2df9fed39b0fbef40af4c43
Sahan Weerakkody <sahanjith93@gmail.com>
2025-03-30 11:56:52 +0530

    configured jest and react testing library

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.test.json',
+    },
+  },
+};
+
+export default config;
